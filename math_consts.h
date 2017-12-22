4331ec8c6c0039b27d95c9882c7544c8c80d61d3
Dave <jdadson@yahoo.com>
2017-12-21 16:32:38 -0800

    math_consts,.h
    math_consts.h

---
+++ math_consts.h
@@ -0,0 +1,17 @@
+#pragma once
+
+namespace dj::math {
+	const double  e        = 2.71828182845904523536;	// Euler's number. e
+	const double  log2_e   = 1.44269504088896340736;	// log2(e)
+	const double  log10_e  = 0.434294481903251827651;	// log10(e)
+	const double  ln_2     = 0.693147180559945309417;	// ln(2)
+	const double  ln_10    = 2.30258509299404568402;	// ln(10)
+	const double  pi       = 3.14159265358979323846;	// pi
+	const double  pi_div_2 = 1.57079632679489661923;	// pi/2
+	const double  pi_div_4 = 0.785398163397448309616;	// pi/4
+	const double  one_div_pi = 0.318309886183790671538;	// 1/pi
+	const double  two_div_pi	 = 0.636619772367581343076;  // 2/pi
+	const double  two_div_sqrt_pi  = 1.12837916709551257390;   // 2/sqrt(pi)
+	const double  sqrt_2			= 1.41421356237309504880;	// sqrt(2)
+	const double  one_div_sqrt_2	= 0.707106781186547524401;  // 1/sqrt(2)
+}
